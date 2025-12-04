db68c0df099e2979b7384ba56951adb353e511dc
achmadnabiillaabas <achmadnabillaabas@gmail.com>
2025-12-04 22:37:42 +0700

    [ADD] struktur project

---
+++ main.php
@@ -0,0 +1,31 @@
+<!doctype html>
+
+<html lang="en">
+
+<head>
+
+<1-- Required meta tags -->
+
+<meta charset="utf-8">
+
+<meta name="viewport" content="width=device-width,
+
+initial-scale=1">
+
+<!-- Bootstrap CSS -->
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.8.2/dist/css/bootstrap.min.css" rel="stylesheet"
+
+integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+<title>Learn Github Action</title>
+
+</head>
+
+<body>
+
+<div class="container mt-4">
+
+<h1><?php echo "Github Action"; ?></h1>
+
+</div>
