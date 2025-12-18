1dc0c479b785487761a286d9fd166973dfb9fbd8
azkawildannn <20azkawildan@gmail.com>
2025-12-18 15:13:03 +0700

    [FIX] forecast

--- tests/FileTypeTest.php
+++ tests/FileTypeTest.php
@@ -3,9 +3,9 @@ use PHPUnit\Framework\TestCase;
 
 class FileTypeTest extends TestCase
 {
-    private $projectFiles = [
+    private $projectFiles = array(
         'forecast/index.php'
-    ];
+    );
 
     public function test_files_exist()
     {
